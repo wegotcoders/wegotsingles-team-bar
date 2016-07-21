41f9c28d94b1d0a1dd29ccfa0f80499ca2606e17
Finn Francis <finn_francis@yahoo.com>
2016-07-21 18:04:44 +0100

    Fixed registration controller to add Customer username of registration
    Did some refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :customers
+  devise_for :customers, controllers: { registrations: 'registrations' }
+
   root 'pages#home'
 
   resources :profiles, :only => [:show, :edit, :update] do
