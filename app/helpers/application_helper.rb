22ac6a99ca757c4ac8d873a5b9e43cec7ffdd3ff
Dafin <dafin@users.noreply.github.com>
2016-07-21 15:51:35 +0100

    Added education level variable attributes in the helper. Corresponding Gherkin test scenarios set up.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,4 +34,18 @@ module ApplicationHelper
       [50, 50]
     ], get_pre_selected_option("distance"))
   end
+
+  def education_level_options
+    options_for_select([
+      ['Education Level...', nil],
+      ['Secondary', 0],
+      ['College', 1],
+      ['Undergraduate', 2],
+      ['Masters', 3],
+      ['PHD/ Post Doctorial', 4],
+      ['Other', 5]
+    ], get_pre_selected_option("education_level"))
+  end
+
+
 end
