2d7d3e7298f9e778971e2ce7214e40b0dd203d76
Finn Francis <finn_francis@yahoo.com>
2016-07-22 14:23:10 +0100

    Changed @received messages to only show the received messges in the inbox

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,7 @@
 class MessagesController < ApplicationController
 
   def index
-    @received_messages = current_customer.messages
+    @received_messages = current_customer.received_messages
   end
 
 end
